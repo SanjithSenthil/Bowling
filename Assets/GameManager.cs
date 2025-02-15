fd0c6a17560cf5bdd99c83a721f8393f8a29f9d9
SanjithSenthil <109128028+SanjithSenthil@users.noreply.github.com>
2025-02-15 00:45:42 -0800

    Added scoring system

---
+++ Assets/GameManager.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private float score = 0;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    private FallTrigger[] pins;
+
+    private void Start() {
+        pins = FindObjectsByType<FallTrigger>(FindObjectsSortMode.None);
+        foreach (FallTrigger pin in pins)
+        {
+            pin.OnPinFall.AddListener(IncrementScore);
+        }
+    }
+
+    private void IncrementScore()
+    {
+        score++;
+        scoreText.text = $"Score: {score}";
+    }
+}
